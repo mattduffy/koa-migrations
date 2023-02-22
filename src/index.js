04e7ad0d6b2976734566153f7b6ee749030e0cd7
Matthew Duffy <mattduffy@gmail.com>
2023-02-21 20:58:37 -0800

    Get started on the Migration class methods.

--- src/index.js
+++ src/index.js
@@ -4,3 +4,55 @@
  * @file src/index.js The package entrypoint.
  */
 
+import fs from 'node:fs/promises'
+import { fileURLToPath } from 'node:url'
+import path from 'node:path'
+import Debug from 'debug'
+import Migration from './migration.js'
+
+function migrations(options = {}, application = {}) {
+  const MIGRATION_FILES_DIR = options?.dir || process.env.MIGRATION_FILES_DIR || 'migrations'
+  const error = Debug('migrations:error')
+  const log = Debug('migrations:log')
+  let app
+  let opts
+  if (options && typeof options.use === 'function') {
+    opts = application
+    app = options
+  } else {
+    opts = options
+    app = application
+  }
+  if (!app || typeof app.use !== 'function') {
+    error('Required app instance not provided')
+    throw new Error('Required app instance not provided')
+  }
+  if (!opts.db) {
+    error('Required path to db client not provided')
+    throw new Error('Required path to db client not provided')
+  }
+  log('Adding the db schema migrations middleware to the app.')
+  log(`koa app root directory: ${app.root}`)
+  const migrationsDir = path.resolve(`${app.root}`, MIGRATION_FILES_DIR)
+  log(`koa migrations directory: ${migrationsDir}`)
+
+  return async function runMigrations(ctx, next) {
+    if (app.env === 'development') {
+      try {
+        const o = {
+          dir: migrationsDir,
+          db: opts.db,
+        }
+        let migrationRunner = new Migration(o)
+        migrationRunner = await migrationRunner.init()
+        await next()
+      } catch (e) {
+        error('Error during migrations')
+      }
+    }
+  }
+}
+
+export {
+  migrations,
+}
---
+++ src/migration.js
@@ -0,0 +1,73 @@
+/**
+ * @module @mattduffy/koa-migrations
+ * @author Matthew Duffy <mattduffy@gmail.com>
+ * @file src/migration.js The Migration class definition file.
+ */
+
+import path from 'node:path'
+import fs from 'node:fs/promises'
+import { fileURLToPath } from 'node:url'
+import EventEmitter from 'node:events'
+import Debug from 'debug'
+
+const error = Debug('migrations:class_error')
+const log = Debug('migrations:class_log')
+
+/**
+ * A class providing db schema migration apply and rollback methods.
+ * @summary A class providing db schema migration apply and rollback methods.
+ * @class Migration
+ * @author Matthew Duffy <mattduffy@gmail.com>
+ */
+
+export default class Migration extends EventEmitter {
+  #migrations = []
+
+  constructor(options = {}) {
+    super()
+    log('Migration constructor')
+    this._dir = options?.dir || null
+    this._dbPath = options?.db || null
+    this._fileExt = options?.fileExt || 'json'
+    this._client = null
+    this.ObjectId = null
+    this._db = null
+  }
+
+  /**
+   * Import the db connection and connect.
+   * @summary Import the db connection and connect.
+   * @async
+   * @author Matthew Duffy <mattduffy@gmail.com>
+   * @throws {Error}
+   * @return {object} Return this instance with db connected
+   */
+  async init() {
+    if (!this._dbPath) {
+      log('Missing db path')
+      throw new Error('Missing db path')
+    }
+    try {
+      // log(this._dbPath)
+      // log(process.cwd())
+      const { client, ObjectId }= await import(this._dbPath)
+      this._client = client
+      this._ObjectId = ObjectId
+      await this._client.connect()
+    } catch (e) {
+      error('Failed to import db connection.')
+      throw new Error('Failed to import db connection.')
+    }
+    try {
+      const files = await fs.readdir(this._dir)
+      log(files)
+      //
+      // loop over the files in dir and parse for migration details
+      //
+    } catch (e) {
+      error(`Failed to read contents of ${this._dir}`)
+      throw new Error(`Failed to read contents of ${this._dir}`)
+    }
+    return this
+  }
+}
